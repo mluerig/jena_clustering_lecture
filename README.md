1e097dc7d07690e35f9c849a3ba278922a9a43f9
mluerig <moritz.luerig@gmail.com>
2024-04-24 23:03:09 +0200

    add files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# jena_clustering_lecture
-Accompanying materials to the lecture: "An introduction to pattern recognition and clustering"
+# Accompanying materials to the lecture: "An introduction to pattern recognition and clustering"
+
+./1.
+
+    mamba create -n clustering_lecture python=3.9
+    mamba activate clustering_lecture
+    pip install jupyter lab matplotlib seaborn scikit-learn scipy pandas opencv-python-headless
---
+++ utils.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+from typing import Tuple
+from scipy import stats
+
+def resize_with_pad(image: np.array, 
+                    new_shape: Tuple[int, int], 
+                    padding_color: Tuple[int] = (255, 255, 255)) -> np.array:
+    """
+    Maintains aspect ratio and resizes with padding.
+    Parameters:
+    - image: Image to be resized.
+    - new_shape: Expected (width, height) of new image.
+    - padding_color: Tuple in BGR of padding color
+    
+    Returns:
+    - image: Resized image with padding
+    """
+    original_shape = (image.shape[1], image.shape[0])
+    ratio = float(max(new_shape))/max(original_shape)
+    new_size = tuple([int(x*ratio) for x in original_shape])
+    image = cv2.resize(image, new_size)
+    delta_w = new_shape[0] - new_size[0]
+    delta_h = new_shape[1] - new_size[1]
+    top, bottom = delta_h//2, delta_h-(delta_h//2)
+    left, right = delta_w//2, delta_w-(delta_w//2)
+    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_CONSTANT, value=padding_color)
+    return image
+
+
+def extract_features(contour, gray, mask):
+    """
+    Extracts geometric and texture features from a given contour and associated ROI in an image.
+    
+    Parameters:
+    - contour: The contour from which geometric features are extracted.
+    - gray: Grayscale image from which the ROI is derived.
+    - mask: Binary mask for the ROI, matching dimensions of `gray`.
+    
+    Returns:
+    - A list of extracted features: area, diameter, circularity, mean, variance, skewness, kurtosis.
+    """
+    # Geometric features
+    area = cv2.contourArea(contour)
+    diameter = int(cv2.minEnclosingCircle(contour)[1] * 2)
+    perimeter = cv2.arcLength(contour, True)
+    circularity = (4 * np.pi * area) / (perimeter ** 2) if perimeter else 0
+
+    # Extract ROI using bounding rectangle for texture analysis
+    x, y, w, h = cv2.boundingRect(contour)
+    roi = gray[y:y+h, x:x+w]
+    roi_mask = np.logical_not(mask[y:y+h, x:x+w])
+
+    # Create a masked array that ignores the background of the ROI
+    masked_array = np.ma.masked_array(roi, mask=roi_mask == 0)
+
+    # Texture features
+    mean = np.ma.mean(masked_array)
+    variance = np.ma.var(masked_array)
+    skewness = stats.skew(masked_array.compressed())
+    kurt = stats.kurtosis(masked_array.compressed())
+
+    # Return all features in a list
+    return [area, diameter, circularity, mean, variance, skewness, kurt]
